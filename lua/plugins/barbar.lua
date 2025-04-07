e1ff99c592445cefe22e1a943894ef6fda622ca3
nar1nari <nar1nari@proton.me>
2025-04-07 16:02:49 +0500

    Add Rust development plugins

---
+++ lua/plugins/barbar.lua
@@ -0,0 +1,30 @@
+return {'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function() vim.g.barbar_auto_setup = false end,
+    config = function()
+        require'barbar'.setup {
+            sidebar_filetypes = {
+    -- Use the default values: {event = 'BufWinLeave', text = '', align = 'left'}
+    NvimTree = true,
+    -- Or, specify the text used for the offset:
+    undotree = {
+      text = 'undotree',
+      align = 'center', -- *optionally* specify an alignment (either 'left', 'center', or 'right')
+    },
+    -- Or, specify the event which the sidebar executes when leaving:
+    ['neo-tree'] = {event = 'BufWipeout'},
+    -- Or, specify all three
+    Outline = {event = 'BufWinLeave', text = 'symbols-outline', align = 'right'},
+  },
+        }
+    end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
+    },
+}
