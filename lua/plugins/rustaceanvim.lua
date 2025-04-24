48eaea93fc8aef930a169bca14ff66bbc6ab193c
nar1nari <nar1nariq@proton.me>
2025-04-24 22:56:13 +0500

    Disable rust-analyzer diagnostics for unlinked files

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -2,5 +2,18 @@ return {
     'mrcjkb/rustaceanvim',
     version = '^5', -- Recommended
     lazy = false, -- This plugin is already lazy
+    config = function ()
+        vim.g.rustaceanvim = {
+            server = {
+                default_settings = {
+                    ['rust-analyzer'] = {
+                        diagnostics = {
+                            disabled = {"unlinked-file"}
+                        }
+                    }
+                }
+            }
+        }
+    end
 }
 
