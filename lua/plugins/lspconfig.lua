809415c4e818a7fe6ddbc9fe44fcd0528fd9b0ea
nar1nari <nar1nariq@proton.me>
2025-11-30 20:25:24 +0500

    Update lspconfig config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,48 +1,42 @@
 return {
     'neovim/nvim-lspconfig',
     config = function()
-        local lspconfig = require('lspconfig')
-        lspconfig.pyright.setup{
-            settings = {
-                python = {
-                    analysis = {
-                        extraPaths = {"."}
-                    }
-                }
-            }
-        }
-        lspconfig.lua_ls.setup{
-            settings = { Lua = {
-                workspace = {
-                    checkThirdParty = false,
-                    library = vim.tbl_deep_extend('force', vim.api.nvim_get_runtime_file("", true), {
-                        "${3rd}/luv/library",
-                        "${3rd}/busted/library",
-                        "/usr/share/awesome/lib",
-                        "/usr/share/lua",
-                    }),
-                },
-                diagnostics = {
-                    globals = {
-                        "awesome",
-                        "awful",
-                        "client",
-                        "screen",
-                        "tag",
-                        "root",
-                    },
-                },
-                runtime = { version = 'LuaJIT' },
-                completion = { callSnippet = "Replace", },
-                telemetry = { enable = false, },
-            }}
-        }
+        vim.lsp.config('clangd', {})
+        vim.lsp.enable('clangd')
 
-        lspconfig.clangd.setup{}
-        -- lspconfig.phpactor.setup{}
-        -- lspconfig.tsserver.setup{}
-        -- lspconfig.html.setup{}
-        -- lspconfig.cssls.setup{}
+        vim.lsp.config('lua_ls', {
+            on_init = function(client)
+                if client.workspace_folders then
+                    local path = client.workspace_folders[1].name
+                    if
+                        path ~= vim.fn.stdpath('config')
+                        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+                    then
+                        return
+                    end
+                end
+
+                client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                    runtime = {
+                        version = 'LuaJIT',
+                        path = {
+                            'lua/?.lua',
+                            'lua/?/init.lua',
+                        },
+                    },
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME
+                        }
+                    }
+                })
+            end,
+            settings = {
+                Lua = {}
+            }
+        })
+        vim.lsp.enable('lua_ls')
     end,
 }
 
